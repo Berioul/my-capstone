10aa886cda2c8b6b6eba0e2171e110ffb4bd5b72
Oualid Sahbani <oualidsahbani@me.com>
2022-04-06 16:38:41 +0200

    Security Fixed

--- backend/src/main/java/com/example/demo/Security/PasswordDoNotExistException.java
+++ backend/src/main/java/com/example/demo/Security/PasswordDoNotExistException.java
@@ -3,6 +3,6 @@ package com.example.demo.Security;
 public class PasswordDoNotExistException extends IllegalStateException {
 
     PasswordDoNotExistException(){
-        super("password do not match");
+        super("password does not match");
     }
 }
