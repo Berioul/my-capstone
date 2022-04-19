2ed56b1c8b8cf9dfa595476c085d73d47030e42e
Oualid Sahbani <oualidsahbani@me.com>
2022-04-19 16:31:42 +0200

    CSS

--- frontend/src/TodoList/TodoCategory.tsx
+++ frontend/src/TodoList/TodoCategory.tsx
@@ -1,5 +1,6 @@
 import {Category} from "./model";
 import TodoItem from "./TodoItem";
+import './TodoCategory.css'
 
 interface TodoCategoryProps{
     category: Category;
@@ -8,11 +9,13 @@ interface TodoCategoryProps{
 export default function TodoCategory(props: TodoCategoryProps){
 
     return(
-        <div>
+        <div className='items'>
+            <div className='itemsBox'>
         <h3>{props.category.name}</h3>
             {props.category.items.map((item,index) => <div key={`${item.subject}-${index}`}>
                 <TodoItem item ={item} onItemListChange={props.onItemListChange} />
             </div>) }
+            </div>
         </div>
     )
 }
